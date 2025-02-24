8917ba16269e537a392802197cf537c488640a9a
AndDev741 <anddesenvolved@gmail.com>
2025-02-24 09:45:36 +0000

    feat: Spring boot initial configuration

---
+++ conflictOfBeans/src/main/java/com/andDev/conflictOfBeans/ConflictOfBeansApplication.java
@@ -0,0 +1,13 @@
+package com.andDev.conflictOfBeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConflictOfBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConflictOfBeansApplication.class, args);
+	}
+
+}
---
+++ conflictOfBeans/src/test/java/com/andDev/conflictOfBeans/ConflictOfBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andDev.conflictOfBeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConflictOfBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
